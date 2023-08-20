4996b14d6bd2094fec8dcc55d1db20497bec5595
Raka Antoro <raka@ibfnet.group>
2023-08-20 19:58:32 +0700

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,51 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "hardhat-gas-reporter";
+
+import "@openzeppelin/hardhat-upgrades";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  mocha: {
+    timeout: 0
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    mumbaipolygon: {
+      url: process.env.MUMBAI_POLYGON_TESTNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGON_API_KEY as string,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+};
+
+export default config;
