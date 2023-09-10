d34cfb0bdb282688b312b4cb4c985165f8df9ecc
Raka Antoro <raka@ibfnet.group>
2023-09-10 23:33:04 +0700

    add bsc chain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,10 +33,16 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    bsctestnet: {
+      url: process.env.BSC_TESTNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   etherscan: {
     apiKey: {
       polygonMumbai: process.env.POLYGON_API_KEY as string,
+      bscTestnet: process.env.BSC_API_KEY as string,
     },
   },
   gasReporter: {
